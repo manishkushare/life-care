688bac4930a14aeb6e1bbd8d50cb68b0a80f6f6b
Manish Kushare <manish.altcampus@gmail.com>
2020-11-07 05:45:07 +0530

    added services

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,11 @@ let listItem = document.querySelectorAll(".list-item");
 // let wrapper = document.querySelector(".wrapper");
 let sticky = document.querySelectorAll(".sticky-list");
 let bookSlot = document.querySelector(".book-slot");
+let musculo = document.querySelector(".muscuskeleton");// to select musculskeleton card-container in service-content
+let neuro = document.querySelector(".nuerological");//to select neuro card-container in servidce section
+let cardio = document.querySelector(".cardio");//to select cardio card-container in service section
+let community = document.querySelector(".community");//to select the community card-container in the service section
+
 
 // hamburger menu
 function handleBars(event) {
@@ -42,9 +47,31 @@ listItem.forEach(item => {
 
 // book-slot to book appointment online
 function bookAppointment(event) {
-    window.open("https://clinicia.com/calendar/book?u=manisha lal");
+    window.location.href = "https://clinicia.com/calendar/book?u=manisha lal";
+}
+// to go to masculoskeleton page
+function gotoMusculoskeletonPage(event) {
+    window.location.href ="musculoskeleton.html";
+}
+
+// to go to neurological page
+function gotoNeuroPage(event) {
+    window.location.href = "neurological.html";
+}
+
+// to go to cardio page
+function gotoCardioPage(event) {
+    window.location.href = "cardio-pulmonary.html";
+}
+// go to community page
+function gotoCommunity(event) {
+    window.location.href = "community.html";
 }
 
 bars.addEventListener("click", handleBars);
 window.addEventListener("scroll", changeColor);
 bookSlot.addEventListener("click", bookAppointment);
+musculo.addEventListener("click", gotoMusculoskeletonPage);
+neuro.addEventListener("click" ,gotoNeuroPage );
+cardio.addEventListener("click", gotoCardioPage);
+community.addEventListener("click", gotoCommunity);
